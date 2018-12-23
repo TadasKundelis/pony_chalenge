b29fa2ffa27293793f671b095c318b08f219bce7
Tadas Kundelis <tkundelis@csc.com>
2018-12-23 03:42:16 +0200

    changed from class component to stateless component

--- src/js/components/Maze.js
+++ src/js/components/Maze.js
@@ -3,41 +3,39 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Cell from './Cell';
 
-class Maze extends React.Component {
-  render() {
-    const {
-      displayMaze, maze, width, height, mazeHelper
-    } = this.props;
+const Maze = (props) => {
+  const {
+    displayMaze, maze, width, height, mazeHelper
+  } = props;
     //calculate css width and height based on maze width and height
-    const [CSSwidth, CSSheight] = [width, height].map(param => `${param * 25 + 3}px`);
-    const styles = {
-      width: CSSwidth,
-      height: CSSheight
-    };
-    let cells;
-    if (maze) {
-      cells = Array.from({ length: width * height }, (_, index) => {
-        const [row, col] = mazeHelper.calculateCoordinates([index]);
-        const data = maze[row][col];
-        return (
-          <Cell {...data} key={index} />
-        );
-      });
-    }
-    return (
-      <React.Fragment>
-        <div className="mazeContainer">
-          {maze && displayMaze
-            ? (
-              <div style={styles} className="maze">
-                { cells }
-              </div>
-            ) : null}
-        </div>
-      </React.Fragment>
-    );
+  const [CSSwidth, CSSheight] = [width, height].map(param => `${param * 25 + 3}px`);
+  const styles = {
+    width: CSSwidth,
+    height: CSSheight
+  };
+  let cells;
+  if (maze) {
+    cells = Array.from({ length: width * height }, (_, index) => {
+      const [row, col] = mazeHelper.calculateCoordinates([index]);
+      const data = maze[row][col];
+      return (
+        <Cell {...data} key={index} />
+      );
+    });
   }
-}
+  return (
+    <React.Fragment>
+      <div className="mazeContainer">
+        {maze && displayMaze
+          ? (
+            <div style={styles} className="maze">
+              { cells }
+            </div>
+          ) : null}
+      </div>
+    </React.Fragment>
+  );
+};
 
 const mapStateToProps = state => ({
   displayMaze: state.UI.displayMaze,
@@ -48,6 +46,7 @@ const mapStateToProps = state => ({
 });
 
 Maze.propTypes = {
+  displayMaze: PropTypes.bool.isRequired,
   maze: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.object)),
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired
