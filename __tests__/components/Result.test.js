740d9977176bfb747de9cafd8fda7372df012b5c
Tadas Kundelis <tkundelis@csc.com>
2018-12-26 00:40:55 +0200

    put prop object in describe block

--- __tests__/components/Result.test.js
+++ __tests__/components/Result.test.js
@@ -3,20 +3,23 @@ import { shallow } from 'enzyme';
 import { Result } from '../../src/js/components/Result';
 
 describe('<Result />', () => {
+  const mockStartNewGame = jest.fn();
+  const data = {
+    display: true,
+    result: 'won',
+    startNewGame: mockStartNewGame
+  };
+  const wrapper = shallow(<Result {...data} />);
   it('should render result prop text', () => {
-    const data = {
-      display: true,
-      result: 'won'
-    };
-    const wrapper = shallow(<Result {...data} />);
     expect(wrapper.find('.result').text()).toBe('won');
   });
   it('should render null if display = false', () => {
-    const data = {
-      display: false,
-      result: 'won'
-    };
+    data.display = false;
     const wrapper = shallow(<Result {...data} />);
     expect(wrapper.type()).toEqual(null);
+  });
+  it('should call startNewGame function when "new game" button is clicked', () => {
+    wrapper.find('button').simulate('click');
+    expect(mockStartNewGame.mock.calls.length).toBe(1);
   });
 });
