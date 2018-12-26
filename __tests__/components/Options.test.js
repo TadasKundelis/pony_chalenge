176eef78ab57833b9f97a691a3a3e94555d94353
Tadas Kundelis <tkundelis@csc.com>
2018-12-26 19:02:25 +0200

    added test for setProp

--- __tests__/components/Options.test.js
+++ __tests__/components/Options.test.js
@@ -5,13 +5,16 @@ import { Select } from '../../src/js/components/Select';
 
 describe('<Options />', () => {
   const mockFetchMazeID = jest.fn();
+  const mockSetProp = jest.fn();
   const data = {
     width: 15,
     height: 15,
     display: true,
     difficulty: 5,
-    fetchMazeID: mockFetchMazeID
+    fetchMazeID: mockFetchMazeID,
+    setProp: mockSetProp
   };
+
   const wrapper = shallow(<Options {...data} />);
   it('should contain 3 Select components', () => {
     expect(wrapper.find(Select)).toHaveLength(3);
