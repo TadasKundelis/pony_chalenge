75679c0295954080b4440d40878251409bcbd144
Tadas Kundelis <tkundelis@csc.com>
2019-01-02 02:12:18 +0200

    corrected indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,41 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "no-multi-spaces": "error",
-        "linebreak-style": ["error", "windows"],
-        "comma-dangle": ["error", "never"],
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
-        "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-        "no-use-before-define": 0,
-        "no-console": 0,
-        "spaced-comment":0,
-        "no-loop-func":0,
-        "import/prefer-default-export":0,
-        "no-case-declarations":0,
-        "class-methods-use-this":0,
-        "no-shadow":0
-    },
-    "env": {
-        "browser": true,
-        "node": true,
-        "jest": true,
-    },
-    "parser": "babel-eslint"
+  "extends": "airbnb",
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "no-multi-spaces": "error",
+    "linebreak-style": ["error", "windows"],
+    "comma-dangle": ["error", "never"],
+    "react/jsx-filename-extension": [1, {
+      "extensions": [".js", ".jsx"]
+    }],
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": true
+    }],
+    "no-unused-vars": ["error", {
+      "argsIgnorePattern": "^_"
+    }],
+    "spaced-comment": 0,
+    "import/prefer-default-export": 0,
+    "no-case-declarations": 0,
+    "no-console": 0,
+    "no-use-before-define": 0,
+    "class-methods-use-this": 0,
+    "no-shadow": 0,
+    "no-restricted-syntax": 0,
+    "jsx-a11y/label-has-for": [2, {
+      "components": ["Label"],
+      "required": {
+        "every": ["id"]
+      },
+      "allowChildren": false
+    }]
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true,
+  },
+  "parser": "babel-eslint"
 };
