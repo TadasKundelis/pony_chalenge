5f0bc5df2c83fde5a399c676a1bd126af6823d2e
TadasKundelis <kundelist@gmail.com>
2019-01-02 02:37:38 +0200

    Update Result.js

--- src/js/components/Result.js
+++ src/js/components/Result.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
 import { resetState } from '../thunks';
 
 //add export here to export unconnected component for testing
