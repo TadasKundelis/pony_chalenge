2a02ab1606d3ec4e05cf0186bc36d6a91d909789
Tadas Kundelis <tkundelis@csc.com>
2019-01-03 19:46:07 +0200

    added general UI action creator for display updates

--- src/js/actions/UI.js
+++ src/js/actions/UI.js
@@ -1,24 +1,39 @@
 import * as actionTypes from '../constants/actionTypes';
 
-export const updateMazeUI = value => ({
-  type: actionTypes.UPDATE_MAZE_UI,
+
+export const updateDisplay = (el, value) => ({
+  type: actionTypes.UPDATE_UI,
+  el,
   value
 });
 
-export const updateOptionsUI = value => ({
-  type: actionTypes.UPDATE_OPTIONS_UI,
-  value
-});
-
-export const updatePlayBtnUI = value => ({
-  type: actionTypes.UPDATE_PLAYBTN_UI,
-  value
-});
-
-export const updateResultUI = value => ({
-  type: actionTypes.UPDATE_RESULT_UI,
-  value
-});
-export const resetUI = () => ({
+export const reset = () => ({
   type: actionTypes.RESET_UI
 });
+export const update = state => (dispatch) => {
+  const updateElement = ([el, value]) => dispatch(updateDisplay(el, value));
+  switch (state) {
+    case 'loading':
+      [
+        ['options', 'hide'],
+        ['spinner', 'display']
+      ].forEach(updateElement);
+      break;
+    case 'loaded':
+      [
+        ['maze', 'display'],
+        ['spinner', 'hide'],
+        ['playBtn', 'display']
+      ].forEach(updateElement);
+      break;
+    case 'playing':
+      updateElement(['playBtn', 'hide']);
+      break;
+    case 'gameOver':
+      updateElement(['result', 'display']);
+      break;
+    default:
+      dispatch(reset());
+      break;
+  }
+};
