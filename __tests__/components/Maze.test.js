f7d24d29a7a02840a458fabfa05dcdef2cfb0de6
Tadas Kundelis <tkundelis@csc.com>
2019-01-06 17:06:30 +0200

    update data in Maze.test.js

--- __tests__/components/Maze.test.js
+++ __tests__/components/Maze.test.js
@@ -5,24 +5,35 @@ import Cell from '../../src/js/components/Cell';
 import createInstance from '../../mock data/MazeHelper';
 
 describe('Maze component', () => {
-  const cells = Array.from({ length: 15 },
-    _ => Array.from({ length: 15 },
-      _ => ({ row: 0, col: 0, walls: [] })));
+  let wrapper;
+  let data;
+  beforeEach(() => {
+    const cells = Array.from({ length: 15 },
+      _ => Array.from({ length: 15 },
+        _ => ({ row: 0, col: 0, walls: [] })));
 
-  const data = {
-    playBtnUI: 'display',
-    matrix: cells,
-    mazeHelper: createInstance(),
-    width: 15,
-    height: 15,
-    mazeUI: 'display'
-  };
-  const wrapper = shallow(<Maze {...data} />);
+    data = {
+      matrix: cells,
+      mazeHelper: createInstance(),
+      width: 15,
+      height: 15,
+      displaySetting: 'displayed'
+    };
+    wrapper = shallow(<Maze {...data} />);
+  });
+
   it('should contain number of cells equal to width * height', () => {
     expect(wrapper.find(Cell)).toHaveLength(225);
-  }); it('should have a mazeContainer CSS class', () => {
-    expect(wrapper.find('.mazeContainer')).toHaveLength(1);
-  }); it('should have a maze CSS class', () => {
+  });
+  it('should have a maze CSS class', () => {
     expect(wrapper.find('.maze')).toHaveLength(1);
+  });
+  it('should have a "displayed" CSS class if displaySetting = "displayed"', () => {
+    expect(wrapper.find('div').hasClass('displayed')).toEqual(true);
+  });
+  it('should have a "hidden" CSS class if displaySetting = "hidden"', () => {
+    data.displaySetting = 'hidden';
+    wrapper = shallow(<Maze {...data} />);
+    expect(wrapper.find('div').hasClass('hidden')).toEqual(true);
   });
 });
